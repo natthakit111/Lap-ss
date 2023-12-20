62afca05bbc3aba52857d0c3c2350b978a43e3d1
natthakit111 <natthakit_ya66@live.ac.th>
2023-12-20 23:58:22 +0700

    Lab 2.1

---
+++ Untitled-1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void reverse( char str1[], char str2[] ) ;
+
+int main() {
+    char text[ 50 ] = "I Love You" ;
+    char out[ 50 ] ;
+    reverse( text, out ) ;
+    return 0 ; 
+}//end function
+
+void reverse( char str1[ ], char str2[ ] ) {
+    int length = 0 ;
+    while ( str1[ length ] != '\0' ) {
+        length++ ;
+    }//end while
+
+    for ( int i = 0 ; i < length ; i++ ) {
+        str2[ i ] = str1[ length - i - 1 ] ;
+    }//end for
+    str2[ length ] = '\0' ;  
+
+    printf( "Original: %s\n", str1 ) ;
+    printf( "Reversed: %s\n", str2 ) ;
+}//end function reverse 
